6dd78f40c8825410bab0250c0acf808e5afaa37e
aek <aekky123806@gmail.com>
2023-05-23 13:53:52 +0700

    edit come code

--- services/jwt_verify.ts
+++ services/jwt_verify.ts
@@ -1,29 +1,30 @@
 import { importX509, jwtVerify } from "jose";
 
 let publicKeys: any;
-const firebaseProjectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+const firebaseProjectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const api: string = process.env.NEXT_PUBLIC_FIREBASE_GOOGLE_API!;
 const getKey = async () => {
-    if (publicKeys) {
-        return publicKeys;
-    }
-    const res = await fetch(
-        `https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com`
-    );
-    publicKeys = await res.json();
+  if (publicKeys) {
     return publicKeys;
-}
+  }
+  const res = await fetch(api);
+  publicKeys = await res.json();
+  return publicKeys;
+};
 export async function verifyFirebaseJwt(firebaseJwt: string) {
-    const publicKeys = await getKey();
-    const decode = await jwtVerify(firebaseJwt, async(header,_alg)=>{
-        const x509Cert = publicKeys[header.kid!]
-        const publicKey = await importX509(x509Cert, "RS256");
-        return publicKey;
+  const publicKeys = await getKey();
+  const decode = await jwtVerify(
+    firebaseJwt,
+    async (header, _alg) => {
+      const x509Cert = publicKeys[header.kid!];
+      const publicKey = await importX509(x509Cert, "RS256");
+      return publicKey;
     },
     {
-        issuer: `https://securetoken.google.com/${firebaseProjectId}`,
-        audience: firebaseProjectId,
-        algorithms: ["RS256"],
-      }
-    )
-    return decode.payload;
+      issuer: `https://securetoken.google.com/${firebaseProjectId}`,
+      audience: firebaseProjectId,
+      algorithms: ["RS256"],
+    }
+  );
+  return decode.payload;
 }
