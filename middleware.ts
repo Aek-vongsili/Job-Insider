6fe7a65affecb6b089871964fd121fb9e8fe23fc
aek <aekky123806@gmail.com>
2024-05-21 18:47:27 +0700

    optimize middleware

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const LOGIN_URL = "/login";
+const HOME_URL = "/";
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  switch (true) {
+    case !token &&
+      (pathname.startsWith("/employers-dashboard") ||
+        pathname.startsWith("/candidates-dashboard")):
+      return NextResponse.redirect(new URL(LOGIN_URL, req.url), 307);
+    case token &&
+      (pathname.startsWith("/login") || pathname.startsWith("/register")):
+      return NextResponse.redirect(new URL(HOME_URL, req.url), 301);
+    default:
+      return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: [
+    "/employers-dashboard/:path*",
+    "/login",
+    "/candidates-dashboard/:path*",
+    "/register",
+  ],
+};
