ed94f2bfccd244d60026083c468e2e04784d295d
aek <aekky123806@gmail.com>
2024-05-22 11:07:02 +0700

    change

--- data/employerMenuData.js
+++ data/employerMenuData.js
@@ -17,7 +17,7 @@ module.exports = [
     id: 3,
     name: "Post a New Job",
     icon: "la-paper-plane",
-    routePath: "/employers-dashboard/dashboard",
+    routePath: "/employers-dashboard/post-jobs",
     active: "",
   },
   {
