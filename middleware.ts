c871d5c8076aad4f081eb9b2b508bd28ec72f9a7
aek <aekky123806@gmail.com>
2024-05-22 15:09:45 +0700

    change

--- middleware.ts
+++ middleware.ts
@@ -13,9 +13,9 @@ export default async function middleware(req: NextRequest) {
       (pathname.startsWith("/employers-dashboard") ||
         pathname.startsWith("/candidates-dashboard")):
       return NextResponse.redirect(new URL(LOGIN_URL, req.url), 307);
-    case token &&
-      (pathname.startsWith("/login") || pathname.startsWith("/register")):
-      return NextResponse.redirect(new URL(HOME_URL, req.url), 307);
+    // case token &&
+    //   (pathname.startsWith("/login") || pathname.startsWith("/register")):
+    //   return NextResponse.redirect(new URL(HOME_URL, req.url), 307);
     default:
       return NextResponse.next();
   }
